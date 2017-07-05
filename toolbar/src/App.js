d89513aeb62fe68dc2fa63032828591e39df4e2a
Urian <urianchang@gmail.com>
2017-07-04 23:56:39 -0700

    Clicking logo will change state of toolbar variable

--- toolbar/src/App.js
+++ toolbar/src/App.js
@@ -6,37 +6,53 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Toolbar />
+        <ToolbarControl />
       </div>
     );
   }
 }
 
-class Toolbar extends Component {
+// <ul>
+//   <li><a href="#"><img src= { logo } className="App-logo" /></a></li>
+//   <li><DashboardMenu /></li>
+//   <li><TLink url="#" title="A" /></li>
+//   <li><TLink url="#" title="B" /></li>
+//   <li><TLink url="#" title="C" /></li>
+// </ul>
+
+class ToolbarControl extends Component {
+
+  constructor() {
+    super();
+    this.state = {isSlideOut: false};
+    this.slide = this.slide.bind(this); //: Need to check to why we must 'bind'
+  }
+
+  slide() {
+    let s = !this.state.isSlideOut;
+    this.setState({
+      isSlideOut: s
+    });
+  }
+
   render() {
+    const isSlideOut = this.state.isSlideOut;
+
+    let toolbar = null;
+    if (isSlideOut) {
+      toolbar = <h1>opened</h1>;
+    } else {
+      toolbar = <h1>closed</h1>;
+    }
+
     return (
-      <div className="navbar">
-        <ul>
-          <li><span><a href="#"><img src= { logo } className="App-logo" /></a></span></li>
-          <li><span><DashboardMenu /></span></li>
-          <li><span><a href="#">Analysis</a></span></li>
-          <li><span><a href="#">Data</a></span></li>
-          <li><span><a href="#">Timeline</a></span></li>
-        </ul>
+      <div className="toolbar">
+        <img src={logo} className="App-logo" alt="logo" onClick={this.slide}/>
+        {toolbar}
       </div>
     );
   }
 }
 
-class DashboardMenu extends Component {
-  render() {
-    return (
-      <ul>
-        <li>hi</li>
-        <li>bye</li>
-      </ul>
-    );
-  }
-}
 
 export default App;
