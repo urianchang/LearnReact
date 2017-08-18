a36700b60e42b7b57be9d590dcd6c5635ef99cc1
Urian <urianchang@gmail.com>
2017-08-17 22:18:52 -0700

    Set up interval that keeps making an API call until jobs are complete

--- volty/src/templates/assemble/AssemblePage.js
+++ volty/src/templates/assemble/AssemblePage.js
@@ -5,15 +5,35 @@ class AssemblePage extends Component {
 
   constructor(props){
     super(props);
+
     this.state = {
       isAssembled: false,
+      count: 0,
     };
+
+    this.checkStatus = this.checkStatus.bind(this);
   };
 
   componentDidMount() {
-    setInterval(fun, 10);
-    function fun() {
-      console.log('from component did Mount');
+    var intervalId = setInterval(this.checkStatus.bind(this), 10);
+    console.log('intervalid:', intervalId);
+    this.setState({ intervalId : intervalId });
+    // console.log("componentDidMount");
+    // this.checkStatus();
+  };
+
+  checkStatus() {
+    if (this.state.count === 100) {
+      clearInterval(this.state.intervalId);
+      this.setState({ isAssembled: true });
+    } else {
+      let URL = "http://localhost:3003/api/v1/execution/" + this.state.count;
+      fetch(URL)
+        .then(res => res.json())
+        .then(json => {
+          console.log("api response received");
+          this.setState({ count: parseInt(json.newCount, 10) });
+        });
     }
   };
 
@@ -25,7 +45,7 @@ class AssemblePage extends Component {
         <h2>Thanks for submitting!</h2>
         <p>This is your execution id (mongoDB ID): { execution_id }</p>
         <p>Status: { execution_status }</p>
-        <h4>Progress Bar goes here</h4>
+        <h4>Progress Bar goes here: { this.state.count }</h4>
       </div>
     );
   }
