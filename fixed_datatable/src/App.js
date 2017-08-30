b01fb4b508334e733970b6ca78239cf9e52652ce
Urian <urianchang@gmail.com>
2017-08-29 18:16:11 -0700

    Added fixed data table of names

--- fixed_datatable/src/App.js
+++ fixed_datatable/src/App.js
@@ -1,19 +1,40 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'fixed-data-table/dist/fixed-data-table.min.css';
+
+const { Table, Column, Cell } = require('fixed-data-table');
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      myTableData: [
+        {name: 'Rylan'},
+        {name: 'Amelia'},
+        {name: 'Estevan'},
+        {name: 'Florence'},
+        {name: 'Tressa'},
+      ],
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Table
+        rowsCount = {this.state.myTableData.length}
+        rowHeight = { 50 }
+        headerHeight = { 50 }
+        width = { 1000 }
+        height = { 300 } >
+        <Column
+          header = { <Cell>Name</Cell> }
+          cell = { props => (
+            <Cell {...props}>
+              { this.state.myTableData[props.rowIndex].name }
+            </Cell>
+          )}
+          width = { 200 }
+        />
+      </Table>
     );
   }
 }
