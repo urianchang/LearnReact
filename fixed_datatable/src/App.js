0162544ad7ffe55a07ff2c913f5882b0772b109f
Urian <urianchang@gmail.com>
2017-08-29 18:29:30 -0700

    Added reusable cells

--- fixed_datatable/src/App.js
+++ fixed_datatable/src/App.js
@@ -3,18 +3,42 @@ import 'fixed-data-table/dist/fixed-data-table.min.css';
 
 const { Table, Column, Cell } = require('fixed-data-table');
 
+//: Create Reusable Cells
+class MyTextCell extends Component {
+  render() {
+    const { rowIndex, field, data, ...props } = this.props;
+    return(
+      <Cell {...props}>
+        { data[rowIndex][field] }
+      </Cell>
+    );
+  }
+}
+
+class MyLinkCell extends Component {
+  render() {
+    const { rowIndex, field, data, ...props } = this.props;
+    const link = data[rowIndex][field];
+    return (
+      <Cell {...props}>
+        <a href={ link }>{ link }</a>
+      </Cell>
+    );
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       myTableData: [
-        {name: 'Rylan'},
-        {name: 'Amelia'},
-        {name: 'Estevan'},
-        {name: 'Florence'},
-        {name: 'Tressa'},
+        {name: 'Rylan', email: "something@gmail.com"},
+        {name: 'Amelia', email: "another@gmail.com"},
+        {name: 'Estevan', email: "anotherone@gmail.com"},
+        {name: 'Florence', email: "pow@hotmail.com"},
+        {name: 'Tressa', email: "helloworld@test.com"},
       ],
-    }
+    };
   }
 
   render() {
@@ -27,11 +51,22 @@ class App extends Component {
         height = { 300 } >
         <Column
           header = { <Cell>Name</Cell> }
-          cell = { props => (
-            <Cell {...props}>
-              { this.state.myTableData[props.rowIndex].name }
-            </Cell>
-          )}
+          cell = {
+            <MyTextCell
+              data = { this.state.myTableData }
+              field = "name"
+            />
+          }
+          width = { 200 }
+        />
+        <Column
+          header={ <Cell>Email</Cell> }
+          cell = {
+            <MyLinkCell
+              data = { this.state.myTableData }
+              field = "email"
+            />
+          }
           width = { 200 }
         />
       </Table>
