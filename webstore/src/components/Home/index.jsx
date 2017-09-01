241fefb13aea1e1a25e357195ca8f5bcab399034
Urian <urianchang@gmail.com>
2017-09-01 15:25:53 -0700

    Able to pull data from Firebase and render in data table

--- webstore/src/components/Home/index.jsx
+++ webstore/src/components/Home/index.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import InventoryTable from '../InventoryTable';
+import './home.css';
 
 export default class Home extends Component {
   render() {
     return(
       <div>
         <h2>Welcome to the Sight Machine Boutique</h2>
+        <h3>Current Inventory:</h3>
         <InventoryTable topic={"test"} />
       </div>
     );
--- webstore/src/components/InventoryTable/index.jsx
+++ webstore/src/components/InventoryTable/index.jsx
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
-import firebase from '../../firebase';
+import firebase from '../../firebase';  // Firebase!
+import './table.css';
+// import 'fixed-data-table/dist/fixed-data-table.min.css';  // Fixed-data-table CSS
+// const { Table, Column, Cell } = require('fixed-data-table'); // Fixed data table
 
 class InventoryTable extends Component {
   constructor(props) {
     super(props);
-    // console.log(`Table topic: ${this.props.topic}`);
     this.state = {
       items: null,
     };
+    this.eachRow = this.eachRow.bind(this);
   }
 
   componentDidMount() {
     const itemsRef = firebase.database().ref('masterSheet');
     itemsRef.on('value', (snapshot) => {
-      console.log(snapshot.val());
+      let items = snapshot.val();
+      //: ["NAME", "TYPE", "ID", "COUNT", "DESCRIPTION", "IMG_URL"]
+      this.setState({ items });
     });
+  }
+
+  eachRow(params, i) {
+    return(
+      <TableRow key={i} index={i} cellInfoArray={params} />
+    );
   }
 
   render() {
@@ -24,9 +35,32 @@ class InventoryTable extends Component {
       );
     } else {
       return(
-        <p>Check the console</p>
+        <table>
+          <tbody>
+            { this.state.items.map(this.eachRow) }
+          </tbody>
+        </table>
       );
     }
+  }
+}
+
+class TableRow extends Component {
+  constructor(props) {
+    super(props);
+    this.makeCell = this.makeCell.bind(this);
+  }
+
+  makeCell(param, i) {
+    return(<td key={i}>{ param }</td>);
+  }
+
+  render() {
+    return(
+      <tr>
+        { this.props.cellInfoArray.map(this.makeCell) }
+      </tr>
+    );
   }
 }
 
