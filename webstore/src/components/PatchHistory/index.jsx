39dcb076a7dec669a3f2aa5387a90546a9e4a1af
Urian <urianchang@gmail.com>
2017-09-13 09:17:24 -0700

    Renamed the PatchHistory file to use the JSX extension

--- webstore/src/components/PatchHistory/index.jsx
+++ webstore/src/components/PatchHistory/index.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import PatchCard from '../PatchCard';
 
 class PatchHistory extends Component {
   render() {
     return(
       <div>
         <h2>History of Patches</h2>
+        <PatchCard />
       </div>
     );
   }
