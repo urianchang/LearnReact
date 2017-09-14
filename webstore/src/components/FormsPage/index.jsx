2086354c1dd282c2aaa4e2ffd1d180f8dad9143f
Urian <urianchang@gmail.com>
2017-09-14 16:02:32 -0700

    Embedded Google forms based on selected link

--- webstore/src/components/FormsPage/index.jsx
+++ webstore/src/components/FormsPage/index.jsx
@@ -1,39 +1,73 @@
 import React from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
-
-const Form = ({ match }) => (
-  <div>
-    <h3>{match.params.topicId}</h3>
-  </div>
-);
+import './forms.css';
 
 const FormsPage = ({ match }) => (
   <div>
-    <h2>Forms</h2>
-    <ul>
-      <li>
-        <Link to={`${match.url}/rendering`}>
-          Rendering with React
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/components`}>
-          Components
-        </Link>
-      </li>
-      <li>
-        <Link to={`${match.url}/props-v-state`}>
-          Props v. State
-        </Link>
-      </li>
-    </ul>
-    <Switch>
-      <Route exact path={match.url} render={() => (
-          <h3>Please select a topic.</h3>
-        )} />
-      <Route path={`${match.url}/:topicId`} component={Form} />
-    </Switch>
+    <div className="forms-sidebar">
+      <h2>Forms</h2>
+      <ul>
+        <li>
+          <Link className="sidebar-link" to={`${match.url}/goldengear`}>
+            Golden Gear Nomination
+          </Link>
+        </li>
+        <li>
+          <Link className="sidebar-link" to={`${match.url}/order`}>
+            Store Order
+          </Link>
+        </li>
+        <li>
+          <Link className="sidebar-link" to={`${match.url}/ideas`}>
+            Product Suggestion
+          </Link>
+        </li>
+      </ul>
+    </div>
+    <div className="forms-container">
+      <Switch>
+        <Route exact path={match.url} render={() => (
+            <h3>Please select a form.</h3>
+          )} />
+        <Route path={`${match.url}/goldengear`} component={ GoldenGearNomination } />
+        <Route path={`${match.url}/order`} component={ OrderForm } />
+        <Route path={`${match.url}/ideas`} component={ IdeaForm } />
+      </Switch>
+    </div>
   </div>
+);
+
+// <Route path={`${match.url}/:formId`} component={ Form } />
+
+//: Google Forms below
+const GoldenGearNomination = () => (
+  <iframe
+    className="google-form"
+    src="https://docs.google.com/forms/d/e/1FAIpQLSdJ5mbjX1FYpiqhET8COlMjXe0Oqq-qhL7vhWONE-RS5nVeow/viewform?embedded=true"
+    title="Golden Gear Nomination Form"
+  >
+    Loading...
+  </iframe>
+);
+
+const OrderForm = () => (
+  <iframe
+    className="google-form"
+    src="https://docs.google.com/forms/d/e/1FAIpQLSeAeHIDUSdF2vyaTnK1ByiGFFvXvwSJIMBtyydXmzQvVA4O7g/viewform?embedded=true"
+    title="Store Order Form"
+  >
+    Loading...
+  </iframe>
+);
+
+const IdeaForm = () => (
+  <iframe
+    className="google-form"
+    src="https://docs.google.com/forms/d/e/1FAIpQLSf7WfY1G_sYccYHK5TFd9FafHja9CZOnZxDR4KixqxCp-OEWA/viewform?embedded=true"
+    title="Store Order Form"
+  >
+    Loading...
+  </iframe>
 );
 
 export default FormsPage;
