6ba6f6cd09c27bb2dc3af34e380e685ebd774899
Urian <urianchang@gmail.com>
2017-09-20 11:59:03 -0700

    Added to hex grid styling and functionality

--- webstore/src/components/HexGrid/index.jsx
+++ webstore/src/components/HexGrid/index.jsx
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
 import './hexGrid.css';
+import duck from '../PatchCard/derpDuck.jpg';
 
 class HexGrid extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
+    this.state = {isHidden: "hidden"}
+    this.showOverlay = this.showOverlay.bind(this);
+    this.hideOverlay = this.hideOverlay.bind(this);
+  }
+
+  showOverlay() {
+    this.setState({isHidden: ""})
+  }
+
+  hideOverlay() {
+    this.setState({isHidden: "hidden"});
   }
 
   render() {
     return(
       <div className="hex-grid">
-        <div className="hexagon-bg">
+        <div
+          className="hexagon-bg"
+          onMouseEnter={this.showOverlay}
+          onMouseLeave={this.hideOverlay}
+          >
           <div className="hexagon">
-            <h3>title</h3>
+            <img src={duck} alt="duck" />
+            <h3 className={this.state.isHidden}>title</h3>
+          </div>
+        </div>
+        <div
+          className="hexagon-bg"
+          onMouseEnter={this.showOverlay}
+          onMouseLeave={this.hideOverlay}
+          >
+          <div className="hexagon">
+            <img src={duck} alt="duck" />
+            <h3 className={this.state.isHidden}>title</h3>
+          </div>
+        </div>
+        <div
+          className="hexagon-bg"
+          onMouseEnter={this.showOverlay}
+          onMouseLeave={this.hideOverlay}
+          >
+          <div className="hexagon">
+            <img src={duck} alt="duck" />
+            <h3 className={this.state.isHidden}>title</h3>
           </div>
         </div>
       </div>
