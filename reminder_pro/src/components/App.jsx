7ece593873b096d5854593da1ca1b62613a2024c
Urian <urianchang@gmail.com>
2018-03-08 16:54:32 -0800

    Added delete reminders functionality and input field for due date of reminder

--- reminder_pro/src/components/App.jsx
+++ reminder_pro/src/components/App.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { addReminder } from '../actions';
+import { addReminder, deleteReminder } from '../actions';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      text: ''
+      text: '',
+      dueDate: ""
     }
   }
 
   addReminder() {
-    this.props.addReminder(this.state.text);
+    console.log('this.state.dueDate', this.state.dueDate);
+    this.props.addReminder(this.state.text, this.state.dueDate);
+  }
+
+  deleteReminder(id) {
+    this.props.deleteReminder(id);
   }
 
   renderReminders() {
@@ -23,7 +29,13 @@ class App extends Component {
           reminders.map(reminder => {
             return(
               <li key={reminder.id} className="list-group-item">
-                <div>{reminder.text}</div>
+                <div className="list-item">{reminder.text}</div>
+                <div
+                  className="list-item delete-button"
+                  onClick={ ()=> this.deleteReminder(reminder.id) }
+                  >
+                  &#x2715;
+                </div>
               </li>
             );
           })
@@ -44,6 +56,11 @@ class App extends Component {
               className="form-control"
               placeholder="I have to..."
               onChange={event => this.setState({text: event.target.value})}
+              />
+            <input
+              className="form-control"
+              type="datetime-local"
+              onChange={event => this.setState({dueDate: event.target.value})}
               />
           </div>
           <button
@@ -67,7 +84,7 @@ function mapStateToProps(state) {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({addReminder}, dispatch);
+  return bindActionCreators({ addReminder, deleteReminder }, dispatch);
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
