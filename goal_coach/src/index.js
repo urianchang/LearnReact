a4a506644666ed2fd0f44e395195f5d9d75d2f38
Urian <urianchang@gmail.com>
2018-07-26 22:59:12 -0700

    Added React Bootstrap and set up basic components

---
+++ goal_coach/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../components/App';
+import SignIn from '../components/SignIn';
+import SignOut from '../components/SignOut';
+
+ReactDOM.render(
+  <div>App</div>, document.getElementById('root')
+);
